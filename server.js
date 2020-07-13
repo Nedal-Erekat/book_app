be9500276e5a4ff0778d8112a982dce7514e7bf4
Nedal_Erekat <nedalerekat@gmail.com>
2020-07-13 15:29:16 +0300

    creat database

--- server.js
+++ server.js
@@ -4,24 +4,40 @@ const express = require('express');
 const superagent = require('superagent');
 
 const app = express();
+const pg = require('pg');//Database
+
 const PORT = process.env.PORT;
+const client = new pg.Client(process.env.DATABASE_URL)
 
-app.use(express.static('./public/styles'));
+ //serve CSS file
+app.use(express.static('./public/styles')); 
 
-
+// modulwear to be able to send our data in a secure way by useing "post" request
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.set('view engine','ejs');
 
-app.get('/',(req,res)=>{
-    res.render('pages/index')
-})
+// Set the view engine so can use EJS templet
+app.set('view engine','ejs');            
+
+app.get('/',loadBooks);
+function loadBooks(req,res) {
+    let SQL = `SELECT * FROM books;`;
+    client.query(SQL)
+    .then(data=>{
+
+        res.render('pages/index',{selectedBooks: data.rows})
+    })
+    
+}
+
+    
+
 app.get('/searches/new',(req,res)=>{
     
     res.render('pages/searches/new');
 })
-app.get('/searches',(req,res)=>{
-    let field=req.query.tit;
+app.get('/searches',(req,res)=>{     //if I used the post handler I have to change "query" to "body"
+    let field=req.query.tit;         // "query" appeare in the main link but "body" will be hide(more secure)
     let searchNam=req.query.name;
 
     let q='';
@@ -33,12 +49,10 @@ app.get('/searches',(req,res)=>{
     let url=`https://www.googleapis.com/books/v1/volumes?q=${q}`;
     superagent.get(url)
     .then(data=>{
-        // console.log(data.body.items);
         let creatBooks=data.body.items.map(ele=>{
             let newBook=new Book(ele);
             return newBook;
         });
-        // res.json(data.body.items);
         res.render('pages/searches/show',{booksData: creatBooks});    
     })
     .catch (error => errorHandler(error));
@@ -63,6 +77,10 @@ function errorHandler(error, request, response) {
     response.status(500).render('pages/error');
 }
 
-app.listen(PORT,()=>{
-    console.log(`Listening on PORT ${PORT}`);
-})
+// To connect the client 
+client.connect()
+    .then(() => {
+        app.listen(PORT, () =>
+            console.log(`listening on ${PORT}`)
+        );
+    })
