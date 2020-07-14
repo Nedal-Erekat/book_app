da9b58d298e5cb8063ce1d6ee65e910915fa3e9d
Nedal_Erekat <nedalerekat@gmail.com>
2020-07-14 13:17:58 +0300

    created rout for details

--- data/books.sql
+++ data/books.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS books (
     id SERIAL PRIMARY KEY,
     author VARCHAR(255),
     title VARCHAR(255),
+    isbn VARCHAR(255),
     image_url VARCHAR(255),
     description TEXT,
     bookshelf VARCHAR(255)
